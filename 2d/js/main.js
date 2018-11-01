4e6a620b95800070195d1cd166b78075f6193d53
Zerhogi <chorba.vitaliy@gmail.com>
2018-11-01 17:47:32 +0200

    Update 2d/js/main.js

--- 2d/js/main.js
+++ 2d/js/main.js
@@ -279,7 +279,7 @@ var canvas = document.querySelector("#scene"),
         
     var colors = ["#0000ff"];
     var img = new Image();
-    img.src = "../img/minilogo.png"
+    img.src = "/img/minilogo.png"
 
 	var copy = document.querySelector("#copy");
 
